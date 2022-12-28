c3c80608319d3f99a2d4c97d37d7a065372d6dce
Eduardo Muchak <eduardomuchak@gmail.com>
2022-12-28 08:48:54 -0300

    feat: :sparkles: adiciona funcionamento e estilizacao iniciais da calculadora

--- src/components/CalcButton/index.tsx
+++ src/components/CalcButton/index.tsx
@@ -4,6 +4,9 @@ import { StyleSheet, Dimensions, TouchableHighlight, Text } from 'react-native';
 interface Props {
   label: string;
   onClick: Function;
+  double?: boolean;
+  triple?: boolean;
+  operation?: boolean;
 }
 
 const styles = StyleSheet.create({
@@ -17,12 +20,53 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#888',
   },
+  operationButton: {
+    fontSize: 40,
+    height: Dimensions.get('window').width / 4,
+    width: Dimensions.get('window').width / 4,
+    padding: 20,
+    textAlign: 'center',
+    borderWidth: 1,
+    borderColor: '#888',
+    color: '#fff',
+    backgroundColor: '#fa8231',
+  },
+  buttonDouble: {
+    fontSize: 40,
+    height: Dimensions.get('window').width / 4,
+    padding: 20,
+    backgroundColor: '#f0f0f0',
+    textAlign: 'center',
+    borderWidth: 1,
+    borderColor: '#888',
+    width: (Dimensions.get('window').width / 4) * 2,
+  },
+  buttonTriple: {
+    fontSize: 40,
+    height: Dimensions.get('window').width / 4,
+    padding: 20,
+    backgroundColor: '#f0f0f0',
+    textAlign: 'center',
+    borderWidth: 1,
+    borderColor: '#888',
+    width: (Dimensions.get('window').width / 4) * 3,
+  },
 });
 
-export const CalcButton = ({ label, onClick }: Props) => {
+export const CalcButton = ({
+  label,
+  onClick,
+  double,
+  triple,
+  operation,
+}: Props) => {
+  const stylesButton = [styles.button];
+  if (double) stylesButton.push(styles.buttonDouble);
+  if (triple) stylesButton.push(styles.buttonTriple);
+  if (operation) stylesButton.push(styles.operationButton);
   return (
     <TouchableHighlight onPress={() => onClick()}>
-      <Text style={styles.button}>{label}</Text>
+      <Text style={stylesButton}>{label}</Text>
     </TouchableHighlight>
   );
 };
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -6,27 +6,40 @@ import { useState } from 'react';
 export default function Main() {
   const [displayValue, setDisplayValue] = useState(0);
 
+  const addDigit = (n: number) => {
+    setDisplayValue(n);
+  };
+
+  const clearMemory = () => {
+    console.warn('AC');
+    setDisplayValue(0);
+  };
+
+  const setOperation = (operation: string) => {
+    console.warn(operation);
+  };
+
   return (
     <View style={styles.container}>
       <CalcDisplay value={displayValue} />
       <View style={styles.buttons}>
-        <CalcButton label="AC" onClick={() => console.warn('AC')} />
-        <CalcButton label="/" onClick={() => console.warn('/')} />
-        <CalcButton label="7" onClick={() => console.warn('7')} />
-        <CalcButton label="8" onClick={() => console.warn('8')} />
-        <CalcButton label="9" onClick={() => console.warn('9')} />
-        <CalcButton label="*" onClick={() => console.warn('*')} />
-        <CalcButton label="4" onClick={() => console.warn('4')} />
-        <CalcButton label="5" onClick={() => console.warn('5')} />
-        <CalcButton label="6" onClick={() => console.warn('6')} />
-        <CalcButton label="-" onClick={() => console.warn('-')} />
-        <CalcButton label="1" onClick={() => console.warn('1')} />
-        <CalcButton label="2" onClick={() => console.warn('2')} />
-        <CalcButton label="3" onClick={() => console.warn('3')} />
-        <CalcButton label="+" onClick={() => console.warn('+')} />
-        <CalcButton label="0" onClick={() => console.warn('0')} />
+        <CalcButton label="AC" onClick={() => clearMemory()} triple />
+        <CalcButton label="/" onClick={() => setOperation('/')} operation />
+        <CalcButton label="7" onClick={() => addDigit(7)} />
+        <CalcButton label="8" onClick={() => addDigit(8)} />
+        <CalcButton label="9" onClick={() => addDigit(9)} />
+        <CalcButton label="*" onClick={() => setOperation('*')} operation />
+        <CalcButton label="4" onClick={() => addDigit(4)} />
+        <CalcButton label="5" onClick={() => addDigit(5)} />
+        <CalcButton label="6" onClick={() => addDigit(6)} />
+        <CalcButton label="-" onClick={() => setOperation('-')} operation />
+        <CalcButton label="1" onClick={() => addDigit(1)} />
+        <CalcButton label="2" onClick={() => addDigit(2)} />
+        <CalcButton label="3" onClick={() => addDigit(3)} />
+        <CalcButton label="+" onClick={() => setOperation('+')} operation />
+        <CalcButton label="0" onClick={() => addDigit(0)} />
         <CalcButton label="." onClick={() => console.warn('.')} />
-        <CalcButton label="=" onClick={() => console.warn('=')} />
+        <CalcButton label="=" onClick={() => console.warn('=')} double />
       </View>
     </View>
   );
